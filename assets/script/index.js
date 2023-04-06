068a25e1255ee30faef2a5007f55bb925a9736b4
tamanchichan <tamanchichan@gmail.com>
2023-04-06 15:30:41 -0500

    find-us created

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    alert('Geolocation is not supported by this browser');
+  };
+});
+
+function showPosition() {
+  const latitude = 49.87850272853492;
+  const longitude = -97.15008011662897;
+
+  mapboxgl.accessToken = 'pk.eyJ1IjoidGFtYW5jaGljaGFuIiwiYSI6ImNsZzU3MHlqaDAwbG4zam15dHMxZTl2dW0ifQ.6E_DrY13I1EStpo4sJ0vKg';
+  
+  let map = new mapboxgl.Map({
+    center: [longitude, latitude],
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    zoom: 15
+  });
+
+  new mapboxgl.Marker().setLngLat([longitude, latitude]).addTo(map);
+  
+  map.addControl(new mapboxgl.NavigationControl());
+};
